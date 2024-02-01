b7f0b291a2a714a469a7a5f254a04a27358007af
deepak427 <62477872+deepak427@users.noreply.github.com>
2024-02-01 18:14:00 +0530

    completed project

--- main.py
+++ main.py
@@ -9,54 +9,54 @@ mp_holistic = mp.solutions.holistic  # Mediapipe Solutions
 # Get realtime webcam feed
 
 cap = cv2.VideoCapture(0)
-# while cap.isOpened():
-#     ret, frame = cap.read()
-#     cv2.imshow('Holistic Model Detections', frame)
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow('Holistic Model Detections', frame)
 
-#     if cv2.waitKey(10) & 0xFF == ord('q'):
-#         break
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 # Make detections from feed
 
-# with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
 
-#     while cap.isOpened():
-#         ret, frame = cap.read()
+    while cap.isOpened():
+        ret, frame = cap.read()
 
-#         # Recolor Feed
-#         image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-#         # Make Detections
-#         results = holistic.process(image)
-#         # print(results.face_landmarks)
+        # Recolor Feed
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Make Detections
+        results = holistic.process(image)
+        # print(results.face_landmarks)
 
-#         # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
+        # face_landmarks, pose_landmarks, left_hand_landmarks, right_hand_landmarks
 
-#         # Recolor image back to BGR for rendering
+        # Recolor image back to BGR for rendering
 
-#         # Draw face landmarks
-#         # mp_drawing.draw_landmarks(frame, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
+        # Draw face landmarks
+        # mp_drawing.draw_landmarks(frame, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)
 
-#         # # Right hand
-#         mp_drawing.draw_landmarks(
-#             frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        # # Right hand
+        mp_drawing.draw_landmarks(
+            frame, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-#         # # Left Hand
-#         mp_drawing.draw_landmarks(
-#             frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
+        # # Left Hand
+        mp_drawing.draw_landmarks(
+            frame, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-#         # # Pose Detections
-#         # mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+        # # Pose Detections
+        # mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
 
-#         cv2.imshow('Raw Webcam Feed', frame)
+        cv2.imshow('Raw Webcam Feed', frame)
 
-#         if cv2.waitKey(10) & 0xFF == ord('q'):
-#             break
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
 
-# cap.release()
-# cv2.destroyAllWindows()
+cap.release()
+cv2.destroyAllWindows()
 
 # Apply styling
 
